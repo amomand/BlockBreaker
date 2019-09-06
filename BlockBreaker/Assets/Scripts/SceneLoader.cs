18d863b30006517a27e354745bc217efb0eae484
Alex Omand <aomand@hotmail.co.uk>
2019-09-06 12:31:37 +0100

    imported scenes

---
+++ BlockBreaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    //private Button _startButton;
+
+    //public void Awake()
+    //{
+    //    _startButton = GameObject.Find("StartButton").GetComponent<Button>();
+    //    _startButton.onClick.AddListener(StartButtonClicked);
+    //}
+
+    //public void OnDestroy()
+    //{
+    //    _startButton.onClick.RemoveAllListeners();
+    // }
+
+    public void StartButtonClicked()
+    {
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
