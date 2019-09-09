cead336b4dc1fbe5a341dada65ca6f0dddbbaae9
Alex Omand <aomand@hotmail.co.uk>
2019-09-09 18:25:37 +0100

    move paddle with floats

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePos = new Vector2(10f, 4f);
+        transform.position = paddlePos;
+    }
+}
